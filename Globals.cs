182b5619f2d1a6c5ff059818e4a1d18ce97e7026
STBlade <dommrogers@gmail.com>
2023-04-06 16:19:50 +0100

    version 4.6.3

--- Globals.cs
+++ Globals.cs
@@ -8,8 +8,8 @@ namespace RelentlessNight
 {
     public class Global
     {
-		internal const string RnVersion = "4.6.2";
-		internal const string RnBuildString = "(Build 20230330-2)";
+		internal const string RnVersion = "4.6.3";
+		internal const string RnBuildString = "(Build 20230406-1)";
 
 		// RN globals representing current mod settings
 		internal static int worldSpinDeclinePercent;
